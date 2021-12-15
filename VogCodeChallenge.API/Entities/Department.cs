4b4e539b6570bf8060229f37af94eeb5404a2a7a
ArtMascareno <75270769+ArtMascareno@users.noreply.github.com>
2021-12-15 16:40:45 -0700

    [Dev] Step 2 - Architecture

---
+++ VogCodeChallenge.API/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Department : Entity
+    {
+        public string Name { get; set; }
+        [Index(IsUnique = true)]
+        [StringLength(450)]
+        public string Address { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
