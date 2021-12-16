7ea86f41ba39ceb34e1bb0ba26f8947f149f0a96
ArtMascareno <75270769+ArtMascareno@users.noreply.github.com>
2021-12-15 21:24:47 -0700

    [Dev] Step 7 - NameList

---
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Alternative ForEach ###");
+            QuestionClass.NamesList.ForEach(item => Console.WriteLine(item));
+
+            Console.WriteLine("### Join ###");
+            string lines = string.Join(Environment.NewLine, QuestionClass.NamesList);
+            Console.Write(lines);
+        }
+    }
+}
