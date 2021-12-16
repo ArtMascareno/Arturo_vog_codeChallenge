ce6fab6703efca36ae972a84de27092cc4107879
ArtMascareno <75270769+ArtMascareno@users.noreply.github.com>
2021-12-15 22:47:17 -0700

    [Dev] Step 8 - AWS

---
+++ VogCodeChallenge.AWS/ProcessingDynamoDBStreams.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+using Amazon.Lambda.Serialization.Json;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace VogCodeChallenge.AWS
+{
+    class ProcessingDynamoDBStreams
+    {
+        private static readonly JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public void ProcessDynamoEvent(DynamoDBEvent dynamoEvent)
+        {
+            Console.WriteLine($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records)
+            {
+                Console.WriteLine($"Event ID: {record.EventID}");
+                Console.WriteLine($"Event Name: {record.EventName}");
+
+                string streamRecordJson = SerializeObject(record.Dynamodb);
+                Console.WriteLine($"DynamoDB Record:");
+                Console.WriteLine(streamRecordJson);
+            }
+
+            Console.WriteLine("Stream processing complete.");
+        }
+
+        private string SerializeObject(object streamRecord)
+        {
+            using (var ms = new MemoryStream())
+            {
+                _jsonSerializer.Serialize(streamRecord, ms);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+}
