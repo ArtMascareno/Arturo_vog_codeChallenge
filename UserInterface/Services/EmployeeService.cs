bddc4837758052dba9aee2d6e0fd9965099ab5f7
ArtMascareno <75270769+ArtMascareno@users.noreply.github.com>
2021-12-15 22:08:39 -0700

    [Fix] Code cleaning

--- UserInterface/Services/EmployeeService.cs
+++ UserInterface/Services/EmployeeService.cs
@@ -12,32 +12,32 @@ namespace UserInterface.Services
 
     public class EmployeeService
     {
-        protected DataBaseContext _context;
-        public EmployeeService(DataBaseContext context)
-        {
-            _context = context;
-        }
+        //protected DataBaseContext _context;
+        //public EmployeeService(DataBaseContext context)
+        //{
+        //    _context = context;
+        //}
 
         public IEnumerable<Employee> GetAll()
         {
-            return _context.Employees.Select(x => x).ToList();
-            //MockData mockData = new MockData();
-            //return mockData.GetEmployees();
+            //return _context.Employees.Select(x => x).ToList();
+            MockData mockData = new MockData();
+            return mockData.GetEmployees();
         }
 
         public IList<Employee> ListAll()
         {
-            return _context.Employees.Select(x => x).ToList();
-            //MockData mockData = new MockData();
-            //return mockData.GetEmployees();
+            //return _context.Employees.Select(x => x).ToList();
+            MockData mockData = new MockData();
+            return mockData.GetEmployees();
         }
         
         public IList<Employee> ListAllByDepartment(int departmentId)
         {
-            return _context.Employees.Where(x => x.DepartmentId == departmentId).ToList();
-            //MockData mockData = new MockData();
-            //var depEmployees = mockData.GetEmployees().Where(x => x.DepartmentId == departmentId).ToList();
-            //return depEmployees;
+            //return _context.Employees.Where(x => x.DepartmentId == departmentId).ToList();
+            MockData mockData = new MockData();
+            var depEmployees = mockData.GetEmployees().Where(x => x.DepartmentId == departmentId).ToList();
+            return depEmployees;
         }
     }
 }
--- VogCodeChallenge.ConsoleApp/Program.cs
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -28,11 +28,9 @@ namespace VogCodeChallenge.ConsoleApp
                 // A. For integer 1,2,3,4 
                 case var expression when int.TryParse(value, out _) ? Convert.ToInt32(value) > 0 && Convert.ToInt32(value) < 5 : false:
                     return $"{Convert.ToInt32(value) * 2}";
-                    break;
                 // B. For integers bigger than 4
                 case var expression when int.TryParse(value, out _) ? Convert.ToInt32(value) > 4 : false:
                     return $"{Convert.ToInt32(value) * 3}";
-                    break;
                 // C. For integers below 1
                 case var expression when int.TryParse(value, out _) ? Convert.ToInt32(value) < 1 : false:
                     Exception ex = new Exception("Value can't be less than 1");
@@ -40,16 +38,12 @@ namespace VogCodeChallenge.ConsoleApp
                 // D. For float values of 1.0f and 2.0f 
                 case var expression when float.TryParse(value, out _) ? float.Parse(value) == 1.0f || float.Parse(value) == 2.0f : false:
                     return $"{3.0f}";
-                    break;
                 // E. For float values of 1.0f and 2.0f 
                 case var expression when value is string:
                     return value.ToUpper();
-                    break;
                 default:
                     return value;
-                    break;
             }
-            return "";
         }
     }
 }
