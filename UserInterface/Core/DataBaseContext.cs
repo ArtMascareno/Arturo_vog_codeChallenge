488fe944cd786c70b8329fc5b7fac59cda55db95
ArtMascareno <75270769+ArtMascareno@users.noreply.github.com>
2021-12-15 21:09:52 -0700

    [Dev] Step 6 - Database

---
+++ UserInterface/Core/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Vog.Domain.Entities;
+
+namespace UserInterface.Core
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
--- UserInterface/Services/EmployeeService.cs
+++ UserInterface/Services/EmployeeService.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserInterface.Core;
 using Vog.Domain.Entities;
 using Vog.Domain.Mock;
 
 namespace UserInterface.Services
 {
+
+
     public class EmployeeService
     {
+        protected DataBaseContext _context;
+        public EmployeeService(DataBaseContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Employee> GetAll()
         {
-            MockData mockData = new MockData();
-            return mockData.GetEmployees();
+            return _context.Employees.Select(x => x).ToList();
+            //MockData mockData = new MockData();
+            //return mockData.GetEmployees();
         }
 
         public IList<Employee> ListAll()
         {
-            MockData mockData = new MockData();
-            return mockData.GetEmployees();
+            return _context.Employees.Select(x => x).ToList();
+            //MockData mockData = new MockData();
+            //return mockData.GetEmployees();
         }
         
         public IList<Employee> ListAllByDepartment(int departmentId)
         {
-            MockData mockData = new MockData();
-            var depEmployees = mockData.GetEmployees().Where(x => x.DepartmentId == departmentId).ToList();
-            return depEmployees;
+            return _context.Employees.Where(x => x.DepartmentId == departmentId).ToList();
+            //MockData mockData = new MockData();
+            //var depEmployees = mockData.GetEmployees().Where(x => x.DepartmentId == departmentId).ToList();
+            //return depEmployees;
         }
     }
 }
